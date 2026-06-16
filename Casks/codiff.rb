fb810d401b2d8679520f829a3d9d73bf964dc8d4
Thomas Kistler <tkistler@linkedin.com>
2026-06-15 22:26:41 -0700

    Release 1.3.0.4

--- Casks/codiff.rb
+++ Casks/codiff.rb
@@ -1,6 +1,6 @@
 cask "codiff" do
-  version "1.3.0.3"
-  sha256 "1fb0953a76a935a25b9ae2314acf06b60cecee06f91e6398c39e2621e215a929"
+  version "1.3.0.4"
+  sha256 "bd8a3227bb08b3440e2f7ca51960ac7d616dce5211882f1a80fc81472d2ec7ea"
 
   url "https://github.com/thomaskistler/codiff/releases/download/v#{version}/Codiff-darwin-arm64-#{version}.zip"
   name "Codiff"
